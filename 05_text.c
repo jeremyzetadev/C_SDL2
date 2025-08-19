9eab4e50c9f07900cd559be34d9582344a62e2d0
jeremy zeta <jeremy_zeta@yahoo.com>
2025-08-19 14:59:16 +0900

    add text

---
+++ 05_text.c
@@ -0,0 +1,165 @@
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_surface.h>
+#include <SDL2/SDL_video.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <time.h>
+#include <stdlib.h>
+
+#define WINDOW_TITLE "05 Text"
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+#define IMAGE_FLAGS IMG_INIT_PNG
+#define TEXT_SIZE 80
+
+struct Game {
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_Texture *background;
+    TTF_Font *text_font;
+    SDL_Color text_color;
+    SDL_Rect text_rect;
+    SDL_Texture *text_image;
+};
+
+
+void game_cleanup(struct Game *game, int exit_status);
+bool load_media(struct Game *game);
+bool sdl_initialize(struct Game *game);
+
+int main(){
+    // Initialize game window and renderer
+    struct Game game = {
+        .window = NULL,
+        .renderer = NULL,
+        .background = NULL,
+        .text_font = NULL,
+        .text_color = {255, 255, 255 ,255},
+        .text_rect = {0, 0, 0, 0},
+        .text_image = NULL,
+    };
+
+    if (!sdl_initialize(&game)){
+        game_cleanup(&game, EXIT_FAILURE);
+    }
+    if (!load_media(&game)){
+        game_cleanup(&game, EXIT_FAILURE);
+    }
+
+    bool isGameRunning = true;
+    while(isGameRunning){
+        SDL_Event event;
+        while(SDL_PollEvent(&event)){
+            switch(event.type){
+                case SDL_QUIT:
+                    game_cleanup(&game, EXIT_SUCCESS);
+                    break;
+                case SDL_KEYDOWN:
+                    switch(event.key.keysym.scancode){
+                        case SDL_SCANCODE_ESCAPE:
+                            game_cleanup(&game, EXIT_SUCCESS);
+                            break;
+                        case SDL_SCANCODE_SPACE:
+                            SDL_SetRenderDrawColor(game.renderer, rand()%256, rand()%256, rand()%255, 255);
+                            break;
+                        default:
+                            break;
+                    }
+                default:
+                    break;
+            }
+        }
+        SDL_RenderClear(game.renderer);
+        SDL_RenderCopy(game.renderer, game.background, NULL, NULL);
+        SDL_RenderCopy(game.renderer, game.text_image, NULL, &game.text_rect);
+        SDL_RenderPresent(game.renderer);
+        SDL_Delay(16);
+    }
+
+    printf("All good!");
+
+    game_cleanup(&game, EXIT_SUCCESS);
+    return 0;
+}
+
+void game_cleanup(struct Game *game, int exit_status){
+    SDL_DestroyTexture(game->text_image);
+    TTF_CloseFont(game->text_font);
+    SDL_DestroyTexture(game->background);
+    SDL_DestroyWindow(game->window);
+    SDL_DestroyRenderer(game->renderer);
+    TTF_Quit();
+    IMG_Quit();
+    SDL_Quit();
+    exit(exit_status);
+}
+
+bool load_media(struct Game *game){
+    game->background = IMG_LoadTexture(game->renderer, "images/background.png");
+    if (!game->background){
+        fprintf(stderr, "Error creating Texture: %s\n", IMG_GetError());
+        return false;
+    }
+
+    game->text_font = TTF_OpenFont("fonts/freesansbold.ttf", TEXT_SIZE);
+    if (!game->text_font){
+        fprintf(stderr, "Error creating Font: %s\n", TTF_GetError());
+        return false;
+    }
+
+    SDL_Surface *surface = TTF_RenderText_Blended(game->text_font, "SDLTEXT", game->text_color);
+    if (!surface){
+        fprintf(stderr, "Error creating Surface: %s\n", SDL_GetError());
+        return false;
+    }
+
+    game->text_rect.w = surface->w;
+    game->text_rect.h = surface->h;
+    game->text_image = SDL_CreateTextureFromSurface(game->renderer, surface);
+    SDL_FreeSurface(surface);
+    if (!game->text_image){
+        fprintf(stderr, "Error creating Image from surface: %s\n", SDL_GetError());
+        return false;
+    }
+
+    return true;
+}
+
+bool sdl_initialize(struct Game *game){
+    if (SDL_Init(SDL_INIT_EVERYTHING)== -1){
+        fprintf(stderr, "Error initializing SDL: %s\n", SDL_GetError());
+        return false;
+    }
+
+    int img_init = IMG_Init(IMAGE_FLAGS);
+    if ((img_init & IMAGE_FLAGS) != IMAGE_FLAGS){
+        fprintf(stderr, "Error initializing SDL_image: %s\n", IMG_GetError());
+        return false;
+    }
+
+    if (TTF_Init() == -1){
+        fprintf(stderr, "Error initializing SDL_image: %s\n", IMG_GetError());
+        return false;
+    }
+
+    game->window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+    if(!game->window){
+        fprintf(stderr, "Error creating window: %s\n", SDL_GetError());
+        return false;
+    }
+
+    game->renderer = SDL_CreateRenderer(game->window, -1, 0);
+    if(!game->renderer){
+        fprintf(stderr, "Error creating renderer: %s\n", SDL_GetError());
+        return false;
+    }
+
+    //setup randomizer
+    srand((unsigned)time(NULL));
+
+    return true;
+}
